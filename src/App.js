c613c908e70a89cd2cf103a97d530e799bc1d285
gajendra.singh <gajendra.singh.1@team.cliffex.com>
2023-04-18 16:09:15 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,31 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Navbar from './Pages/Navbar'
 import About from './routes/About'
 import Contact from './routes/Contact'
-// import Home from './routes/Home '
 import Service from './routes/Service'
 import Home from './routes/Home';
 import Hero from './Components/Hero';
-// import Navbar from './Components/Pages/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-     
-        <Router>
-          
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/about' element={<About/>} />
-            <Route path='/contact' element={<Contact/>} />
-            <Route path='/service' element={<Service/>} />
-          </Routes>
-         
-        </Router>
-     
+
+      <Router>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/service' element={<Service />} />
+        </Routes>
+
+      </Router>
+
     </div>
   );
 }
